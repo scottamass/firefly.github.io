262b11317feef3a432dd52e4036e0a0215c77d84
scottamass <scot.atkinson@live.co.uk>
2021-09-08 23:02:11 +0100

    navfix

--- main.js
+++ main.js
@@ -16,12 +16,12 @@ function myFunction() {
    parallax.style.backgroundPositionY = offset *0.9 +"px";
  }
                         )
- const toggleButton = document.getElementsByClassName('toggle-button') [0]
+ const toggleButton =document.getElementsByClassName('toggle-button') [0]
  const navbarLinks =document.getElementsByClassName('navbar-links')[0]
  const navbar =document.getElementsByClassName('navbar')[0]
  
- 
- 
+
+
  toggleButton.addEventListener('click',() => {
        navbarLinks.classList.toggle('active')
    navbar.classList.toggle('active')
